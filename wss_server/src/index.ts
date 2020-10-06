eb553cfa75daee1cd9be56ccfcf2a588ba39b33e
Bruno Leonardo Michels <brunolm7@gmail.com>
2020-10-06 11:21:50 -0300

    [wss] Add first message

--- wss_server/src/index.ts
+++ wss_server/src/index.ts
@@ -1,10 +1,15 @@
 import * as socketIO from 'socket.io'
 import { config } from './config'
+import * as chalk from 'chalk'
 
 const io = socketIO()
 
 io.on('connection', (client) => {
-  console.log(`Got connection from`, client)
+  console.log(chalk.greenBright`Got a connection from`, client.id)
+
+  client.on('initialize', (message) => {
+    console.log(chalk.blueBright`Got a message from`, client.id, 'message:', message)
+  })
 })
 
 console.log(`listening on port`, config.port)
