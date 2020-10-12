6be4b37fc39ed419981ddab1d0e875329e0fe9e9
Ionut Iorgulescu <ionut.iorgulescu@digitalmob.ro>
2020-10-12 18:41:37 +0300

    Add dotenv lib

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 export const config = {
   port: +process.env.PORT || 3000,
   lotus: {
-    api: process.env.LOTUS_API || 'http://ec2-3-137-141-6.us-east-2.compute.amazonaws.com:1234/rpc/v0', // change to localhost
+    api: process.env.LOTUS_API || 'http://3.236.16.245:1234/rpc/v0', // change to localhost
     token:
       process.env.LOTUS_TOKEN ||
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.lbKNdZrj1rPbPaeCOBANmMRL7m39SbpfNFuX7qmFcCg',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.fTZF8M_x5igkL281jLSbIODy9LqOyKjzC2o6EEy0ldQ',
   },
   db: {
     host: process.env.DB_HOST,
