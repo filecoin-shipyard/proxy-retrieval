3c9d3338c6ea10657a3a969c025fbcb01742a00e
Ionut Iorgulescu <ionut.iorgulescu@digitalmob.ro>
2020-10-14 09:56:25 +0300

    remove lotus api and token from config

--- src/config/env.ts
+++ src/config/env.ts
@@ -32,10 +32,8 @@ export const env = {
   port: +process.env.PORT || 3000,
 
   lotus: {
-    api: process.env.LOTUS_API || 'http://3.236.16.245:1234/rpc/v0', // change to localhost
-    token:
-      process.env.LOTUS_TOKEN ||
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.fTZF8M_x5igkL281jLSbIODy9LqOyKjzC2o6EEy0ldQ',
+    api: process.env.LOTUS_API,
+    token: process.env.LOTUS_TOKEN,
   },
 
   db: {
