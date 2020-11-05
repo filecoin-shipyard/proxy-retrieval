d76d3ec31da0ff076415b48baa35baa6c69957ca
aistoc <andreea_istoc@yahoo.com>
2020-11-05 17:39:58 +0200

    #113 Raise chunk_size to 1 MiB

--- src/services/send-chunk.ts
+++ src/services/send-chunk.ts
@@ -11,7 +11,7 @@ import * as lotus from './lotus'
 import { sha256 } from './sha256'
 import { sha256File } from './sha256-file'
 
-const chunkSize = 1024 // * 1024 * 1 // 1 MB
+const chunkSize = 1048576 // 1 MiB
 
 export const sendChunk = async (io: socketIO.Server | socketIO.Socket, message) => {
   try {
